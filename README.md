8f7a957bc0deb83999bd1377dcd913e709725e56
Darya Morozova <fitosmd@yandex.ru>
2020-09-15 19:06:08 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/3h8jg81v3vg2mfc0?svg=true)](https://ci.appveyor.com/project/daryamorozova/auto-2-1selenide)
---
+++ src/test/java/ru/netology/CallbackTest.java
@@ -0,0 +1,70 @@
+package ru.netology;
+
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CallbackTest {
+
+    private WebDriver driver;
+    ChromeOptions options = new ChromeOptions();
+
+    @BeforeAll
+    static void setUpAll() {
+        if (System.getProperty("os.name").equals("Linux")){
+            System.setProperty("webdriver.chrome.driver", "./driver/linux/chromedriver");
+        } else if (System.getProperty("os.name").contains("Windows")) {
+            System.setProperty("webdriver.chrome.driver", "./driver/windows/chromedriver.exe");
+        }
+    }
+
+    @BeforeEach
+    void setUp() {
+        options.setHeadless(true);
+        driver = new ChromeDriver(options);
+    }
+
+    @AfterEach
+    void tearDown() {
+        driver.quit();
+        driver = null;
+    }
+
+    @Test
+    void shouldTestFormCorrect() {
+        driver.get("http://localhost:9999");
+        List<WebElement> elements = driver.findElements(By.className("input__control"));
+        elements.get(0).sendKeys("Игорь Попов");
+        elements.get(1).sendKeys("+79875556565");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.className("button__content")).click();
+
+        String text = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText();
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
+    }
+
+    @Test
+    void shouldTestFormNotCorrect() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Igor Popov");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79875556565");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.className("button__content")).click();
+        String text = driver.findElement(By.className("input__sub")).getText();
+        assertEquals("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.", text.trim());
+    }
+
+
+
+}
